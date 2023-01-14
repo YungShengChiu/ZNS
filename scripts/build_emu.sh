69bd359e88b8ecbb91fb353bee1ffa1f1beec59b
Wilson <WilsonHWS159>
2023-01-14 10:37:56 +0800

    Add a script for building QEMU

---
+++ scripts/build_emu.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Variable
+QEMU_VERSION=7.1.0
+ROOT_DIR=$(realpath $(dirname $(dirname $0)))
+
+# Install dependencies
+apt install -y gcc git libglib2.0-dev libfdt-dev libpixman-1-dev zlib1g-dev ninja-build
+apt install -y libnfs-dev libiscsi-dev
+
+apt install -y libaio-dev libbluetooth-dev libcapstone-dev libbrlapi-dev libbz2-dev
+apt install -y libcap-ng-dev libcurl4-gnutls-dev libgtk-3-dev
+apt install -y libibverbs-dev libjpeg8-dev libncurses5-dev libnuma-dev
+apt install -y librbd-dev librdmacm-dev
+apt install -y libsasl2-dev libsdl2-dev libseccomp-dev libsnappy-dev libssh-dev
+apt install -y libvde-dev libvdeplug-dev libvte-2.91-dev libxen-dev liblzo2-dev
+apt install -y valgrind xfslibs-dev
+
+# Get source code
+cd ${ROOT_DIR}
+[ ! -d ${ROOT_DIR}/emulator ] && mkdir emulator
+cd emulator
+if [ ! -d ${ROOT_DIR}/emulator/qemu-${QEMU_VERSION} ]; then
+	wget https://download.qemu-project.org/qemu-${QEMU_VERSION}.tar.xz
+	tar xvJf qemu-${QEMU_VERSION}.tar.xz
+fi
+
+# Build
+cd qemu-${QEMU_VERSION}
+[ ! -d ${ROOT_DIR}/emulator/qemu-${QEMU_VERSION}/build ] && mkdir build
+cd build
+[ ! -f ${ROOT_DIR}/emulator/qemu-${QEMU_VERSION}/build/qemu-system-x86_64.p ] && ../configure
+[ ! -f ${ROOT_DIR}/emulator/qemu-${QEMU_VERSION}/build/qemu-system-x86_64 ] && make -j$(($(nproc)-1))
+
+# Check
+echo "Done"
+${ROOT_DIR}/emulator/qemu-${QEMU_VERSION}/build/qemu-system-x86_64 -version
