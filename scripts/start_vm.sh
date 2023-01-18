086a69017842aa4a84d4a946865edd4bf6642fe9
WilsonHWS159 <n96101189@gs.ncku.edu.tw>
2023-01-18 21:03:35 +0800

    Check if the iso file exist before virtual machine start

--- scripts/start_vm.sh
+++ scripts/start_vm.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Variables
+HOST_SSH_PORT=22
 ROOT_DIR=$(realpath $(dirname $(dirname $0)))
 QEMU_VERSION=7.1.0
 QEMU_DIR=${ROOT_DIR}/emulator/qemu-${QEMU_VERSION}/build
@@ -11,6 +12,7 @@ VM_SIZE=32G
 VM_MEM=8192
 VM_NPROC=$(nproc)
 VM_SSH_PORT=2222
+VM_VNC_PORT=2
 
 ZNS_IMG=zns0.raw
 ZNS_SIZE=32G
@@ -46,26 +48,32 @@ else
 	echo "ZNS image ${ZNS_IMG} not found!"
 fi
 
+
 # Create and running a virtual machine
 if [ ! -f ${ROOT_DIR}/${VM_IMG} ]; then
-	echo "Creating ${VM_IMG}"
-	${QEMU_DIR}/qemu-img create -f ${VM_FORMAT} ${VM_IMG} ${VM_SIZE}
-	echo "You can now access ${VM_IMG} by VNC with ip address 127.0.0.1:5902"
-	${QEMU_DIR}/qemu-system-x86_64 \
-		-hda ${VM_IMG} \
-		-boot d \
-		-cdrom ${VM_ISO} \
-		-m ${VM_MEM} \
-		-smp ${VM_NPROC} \
-		-cpu host \
-		--enable-kvm \
-		-net user \
-		-net nic \
-		-vnc :2
+	if [ -f ${ROOT_DIR}/${VM_ISO} ]; then
+		echo "Creating ${VM_IMG}"
+		${QEMU_DIR}/qemu-img create -f ${VM_FORMAT} ${VM_IMG} ${VM_SIZE}
+		echo "You can now access ${VM_IMG} via ip address 127.0.0.1"
+		echo "Port ${VM_VNC_PORT} for VNC"
+		${QEMU_DIR}/qemu-system-x86_64 \
+			-hda ${VM_IMG} \
+			-boot d \
+			-cdrom ${VM_ISO} \
+			-m ${VM_MEM} \
+			-smp ${VM_NPROC} \
+			-cpu host \
+			--enable-kvm \
+			-net user \
+			-net nic \
+			-vnc :${VM_VNC_PORT}
+	else
+		echo "${VM_ISO} not found!"
+	fi
 elif [ ${ZNS_IMG_VALID} ]; then
-	echo "Access ${VM_IMG} with ip address 127.0.0.1:5902"
-	echo "Port 5902 for VNC"
-	echo "Port 2222 for SSH"
+	echo "Access ${VM_IMG} via ip address 127.0.0.1"
+	echo "Port ${VM_VNC_PORT} for VNC"
+	echo "Port ${VM_SSH_PORT} for SSH"
 	${QEMU_DIR}/qemu-system-x86_64 \
 		-hda ${VM_IMG} \
 		-m ${VM_MEM} \
@@ -75,9 +83,9 @@ elif [ ${ZNS_IMG_VALID} ]; then
 		-device nvme,id=${NVME_ID},serial=${NVME_SERIAL},zoned.zasl=${NVME_ZONED_ZASL} \
 		-drive file=${ZNS_IMG},id=${ZNS_NVME_ID},format=${ZNS_NVME_FORMAT},if=none \
 		-device nvme-ns,drive=${ZNS_NVME_ID},bus=${NVME_ID},nsid=${ZNS_NVME_NSID},logical_block_size=${ZNS_NVME_LOGICAL_BLK},physical_block_size=${ZNS_NVME_PHYSICAL_BLK},zoned=true,zoned.zone_size=${ZNS_NVME_ZONE_SIZE},zoned.zone_capacity=${ZNS_NVME_ZONE_CAPACITY},zoned.max_open=${ZNS_NVME_MAX_OPEN},zoned.max_active=${ZNS_NVME_MAX_ACTIVE},uuid=${ZNS_NVME_UUID} \
-		-net user,hostfwd=tcp::${VM_SSH_PORT}-:22 \
+		-net user,hostfwd=tcp::${VM_SSH_PORT}-:${HOST_SSH_PORT} \
 		-net nic \
-		-vnc :2
+		-vnc :${VM_VNC_PORT}
 else
 	echo "ZNS image ${ZNS_IMG} not found!"
 fi
