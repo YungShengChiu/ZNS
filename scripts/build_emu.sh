8ba8ed3be94f59797da789aff79c299ea3a47716
WilsonHWS159 <n96101189@gs.ncku.edu.tw>
2023-02-05 19:26:42 +0800

    Separate variables from these scripts

--- scripts/build_emu.sh
+++ scripts/build_emu.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Variable
-QEMU_VERSION=7.1.0
-ROOT_DIR=$(realpath $(dirname $(dirname $0)))
-
 # Install dependencies
 apt install -y gcc git libglib2.0-dev libfdt-dev libpixman-1-dev zlib1g-dev ninja-build
 apt install -y libnfs-dev libiscsi-dev
@@ -16,6 +12,9 @@ apt install -y libsasl2-dev libsdl2-dev libseccomp-dev libsnappy-dev libssh-dev
 apt install -y libvde-dev libvdeplug-dev libvte-2.91-dev libxen-dev liblzo2-dev
 apt install -y valgrind xfslibs-dev
 
+ROOT_DIR=$(realpath $(dirname $(dirname $0)))
+. ${ROOT_DIR}/scripts/config
+
 # Get source code
 cd ${ROOT_DIR}
 [ ! -d ${ROOT_DIR}/emulator ] && mkdir emulator
@@ -23,14 +22,17 @@ cd emulator
 if [ ! -d ${ROOT_DIR}/emulator/qemu-${QEMU_VERSION} ]; then
 	wget https://download.qemu-project.org/qemu-${QEMU_VERSION}.tar.xz
 	tar xvJf qemu-${QEMU_VERSION}.tar.xz
+	rm qemu-${QEMU_VERSION}.tar.xz
 fi
 
 # Build
 cd qemu-${QEMU_VERSION}
 [ ! -d ${ROOT_DIR}/emulator/qemu-${QEMU_VERSION}/build ] && mkdir build
 cd build
-[ ! -f ${ROOT_DIR}/emulator/qemu-${QEMU_VERSION}/build/qemu-system-x86_64.p ] && ../configure
-[ ! -f ${ROOT_DIR}/emulator/qemu-${QEMU_VERSION}/build/qemu-system-x86_64 ] && make -j$(($(nproc)-1))
+if [ ! -f ${ROOT_DIR}/emulator/qemu-${QEMU_VERSION}/build/qemu-system-x86_64 ]; then
+    ../configure
+    make -j$(($(nproc)-1))
+fi
 
 # Check
 echo "Done"
--- scripts/start_vm.sh
+++ scripts/start_vm.sh
@@ -1,38 +1,7 @@
 #!/bin/bash
 
-# Variables
-HOST_SSH_PORT=22
 ROOT_DIR=$(realpath $(dirname $(dirname $0)))
-QEMU_VERSION=7.1.0
-QEMU_DIR=${ROOT_DIR}/emulator/qemu-${QEMU_VERSION}/build
-VM_FORMAT=qcow2
-VM_ISO=ubuntu-20.04.5-desktop-amd64.iso
-VM_IMG=ubuntu_zns.img
-VM_SIZE=32G
-VM_MEM=8192
-VM_NPROC=$(nproc)
-VM_SSH_PORT=2222
-VM_VNC_PORT=2
-
-ZNS_IMG=zns0.raw
-ZNS_SIZE=32G
-
-ZNS_IMG_VALID=false
-
-NVME_ID=nvme0
-NVME_SERIAL=deadbeef
-NVME_ZONED_ZASL=5
-ZNS_NVME_ID=nvmezns0
-ZNS_NVME_FORMAT=raw
-ZNS_NVME_NSID=1
-ZNS_NVME_LOGICAL_BLK=4096
-ZNS_NVME_PHYSICAL_BLK=4096
-ZNS_NVME_ZONE_SIZE=64M
-ZNS_NVME_ZONE_CAPACITY=62M
-ZNS_NVME_MAX_OPEN=16
-ZNS_NVME_MAX_ACTIVE=32
-ZNS_NVME_UUID=5e40ec5f-eeb6-4317-bc5e-c919796a5f79
-
+. ${ROOT_DIR}/scripts/config
 cd ${ROOT_DIR}
 
 # Create a backstore file
