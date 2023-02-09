b1dd5d49b5d50e9dace29a19c549309564d20b37
WilsonHWS159 <n96101189@gs.ncku.edu.tw>
2023-02-09 19:29:36 +0800

    Fix typo

--- scripts/vm_zns/build_zns_env.sh
+++ scripts/vm_zns/build_zns_env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Install dependices and tools
+# Install dependencies and tools
 apt install -y gcc git fio
 apt install -y util-linux lsscsi nvme-cli sg3-utils
 apt install -y libblkid1 libblkid-dev libzbd2 libzbd-dev zbd-utils
