7130ec14e382002fd0aff1844f6d6ae27f3c26a8
WilsonHWS159 <n96101189@gs.ncku.edu.tw>
2023-02-18 21:07:56 +0800

    Modify scripts for building `spdk`

--- scripts/vm_zns/build_zns_env.sh
+++ scripts/vm_zns/build_zns_env.sh
@@ -5,7 +5,21 @@ apt install -y gcc git fio make meson
 apt install -y util-linux lsscsi nvme-cli sg3-utils
 apt install -y libblkid1 libblkid-dev libzbd2 libzbd-dev zbd-utils libnvme-dev
 
+# Download submodule
+git submodule update --init
+
 ROOT_DIR=$(realpath $(dirname $(dirname $(dirname $0))))
+. ${ROOT_DIR}/scripts/vm_zns/config
+
+# Install SPDK
+cd ${ROOT_DIR}/spdk
+git checkout v${SPDK_VERSION}
+git submodule update --init
+scripts/pkgdep.sh --all
+./configure
+make -j$(($(nproc)-1))
+./test/unit/unittest.sh
+make install
 
 # Verify ZNS
 echo "Listing all the NVMe devices"
