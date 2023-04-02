1e8a24b8ea0762e29acd9a2e399f83129269ffd3
WilsonHWS159 <n96101189@gs.ncku.edu.tw>
2023-04-02 22:19:37 +0800

    Implement queue for I/O buffer Implement basic queue operations and define some structures of queue

---
+++ lib/io_buffer_queue.c
@@ -0,0 +1,63 @@
+#include "io_buffer_queue.h"
+
+io_buffer_q_desc_t *io_buffer_q_new(uint32_t q_id, size_t q_depth_max, size_t buffer_max)
+{
+    io_buffer_q_desc_t *q_desc = (io_buffer_q_desc_t *)malloc(sizeof(io_buffer_q_desc_t));
+    
+    if (q_desc) {
+        q_desc->q_id = q_id;
+        q_desc->q_depth = 0;
+        q_desc->q_depth_max = q_depth_max;
+        q_desc->buffer_max = buffer_max;
+        CIRCLEQ_INIT(&q_desc->q_head);
+    }
+    
+    return q_desc;
+}
+
+int io_buffer_q_enqueue(io_buffer_q_desc_t *q_desc, void *arg)
+{
+    if (!q_desc)
+        return 1;
+    if (!arg)
+        return 2;
+    
+    q_entry_t *q_entry = (q_entry_t *)malloc(sizeof(q_entry_t));
+    if (!q_entry)
+        return 3;
+    q_entry->payload = arg;
+    q_entry->q_desc_p = q_desc;
+    
+    CIRCLEQ_INSERT_HEAD(&q_desc->q_head, q_entry, q_entry_p);
+    q_desc->q_depth++;
+    return 0;
+}
+
+int io_buffer_q_dequeue(io_buffer_q_desc_t *q_desc, q_entry_t **q_entry)
+{
+    if (!q_desc)
+        return 1;
+    if (CIRCLEQ_EMPTY(&q_desc->q_head))
+        return 2;
+    
+    *q_entry = CIRCLEQ_LAST(&q_desc->q_head);
+    io_buffer_q_remove(*q_entry);
+    q_desc->q_depth--;
+    return 0;
+}
+
+int io_buffer_q_free(io_buffer_q_desc_t *q_desc)
+{
+    if (!q_desc)
+        return -1;
+    
+    int rc;
+    for (q_entry_t *q_entry; !CIRCLEQ_EMPTY(&q_desc->q_head); spdk_free(io_buffer_q_release_entry(q_entry))) {
+        rc = io_buffer_q_dequeue(q_desc, &q_entry);
+        if (rc)
+            return rc;
+    }
+    
+    free(q_desc);
+    return 0;
+}
---
+++ lib/io_buffer_queue.h
@@ -0,0 +1,56 @@
+#include <spdk/stdinc.h>
+#include <spdk/queue.h>
+#include <spdk/env.h>
+
+#ifndef IO_BUFFER_QUEUE_H
+#define IO_BUFFER_QUEUE_H
+
+/*
+ *      io_buffer_queue is implement by Linux CIRCLEQ
+ **/
+
+typedef struct q_entry_t q_entry_t;
+typedef struct io_buffer_q_desc_t io_buffer_q_desc_t;
+
+struct q_entry_t{
+    CIRCLEQ_ENTRY(q_entry_t) q_entry_p;
+    io_buffer_q_desc_t *q_desc_p;
+    void *payload;
+};
+
+CIRCLEQ_HEAD(q_head_t, q_entry_t);
+struct io_buffer_q_desc_t {
+    struct q_head_t q_head;
+    uint32_t q_id;
+    size_t q_depth;
+    size_t q_depth_max;
+    size_t buffer_max;
+};
+
+
+io_buffer_q_desc_t *io_buffer_q_new(uint32_t q_id, size_t q_depth_max, size_t buffer_max);
+
+int io_buffer_q_enqueue(io_buffer_q_desc_t *q_desc, void *arg);
+
+int io_buffer_q_dequeue(io_buffer_q_desc_t *q_desc, q_entry_t **q_entry);
+
+int io_buffer_q_free(io_buffer_q_desc_t *q_desc);
+
+static inline void *io_buffer_q_release_entry(q_entry_t *q_entry)
+{
+    void *entry_payload = q_entry->payload;
+    free(q_entry);
+    return entry_payload;
+}
+
+static inline void io_buffer_q_remove(q_entry_t *q_entry)
+{
+    CIRCLEQ_REMOVE(&q_entry->q_desc_p->q_head, q_entry, q_entry_p);
+}
+
+static inline void io_buffer_q_insert_front(q_entry_t *q_entry)
+{
+    CIRCLEQ_INSERT_HEAD(&q_entry->q_desc_p->q_head, q_entry, q_entry_p);
+}
+
+#endif
