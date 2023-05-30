f717c225291fdc059ec50d04aa2e648e7b0fbf27
WilsonHWS159 <n96101189@gs.ncku.edu.tw>
2023-05-30 21:24:18 +0800

    Remove submodule

--- scripts/vm_zns/build_zns_env.sh
+++ scripts/vm_zns/build_zns_env.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
 # Install dependencies and tools
-apt install -y gcc git fio make meson
+apt install -y gcc git vim fio make meson
 apt install -y util-linux lsscsi nvme-cli sg3-utils
 apt install -y libblkid1 libblkid-dev libzbd2 libzbd-dev zbd-utils libnvme-dev
-
-# Download submodule
-git submodule update --init
 
 ROOT_DIR=$(realpath $(dirname $(dirname $(dirname $0))))
 . ${ROOT_DIR}/scripts/vm_zns/config
 
-# Install SPDK
-cd ${ROOT_DIR}/spdk
-git checkout v${SPDK_VERSION}
-git submodule update --init
-scripts/pkgdep.sh --all
-./configure
-make -j$(($(nproc)-1))
-./test/unit/unittest.sh
-make install
+# Download and install SPDK libraries
+if [ ! -d ${ROOT_DIR}/spdk ]; then
+    git clone https://github.com/spdk/spdk.git
+    cd ${ROOT_DIR}/spdk
+    git submodule update --init
+    scripts/pkgdep.sh --all
+    git checkout v${SPDK_VERSION}
+    scripts/pkgdep.sh --all
+    ./configure
+    make -j$(nproc)
+    ./test/unit/unittest.sh
+    make install
+fi
 
 # Verify ZNS
 echo "Listing all the NVMe devices"
